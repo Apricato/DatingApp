fe90364de806f31e4f6709e71a126c6caa5e505f
Apricato <100396000+Apricato@users.noreply.github.com>
2025-09-15 06:26:07 -0600

    Parcial01-Add first EF migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,18 @@
+
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 var app = builder.Build();
 
 
@@ -12,3 +21,4 @@ var app = builder.Build();
 app.MapControllers();
 
 app.Run();
+
