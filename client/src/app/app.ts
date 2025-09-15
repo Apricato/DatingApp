db9e855c0fbd30fce4c91604224e5e5bedf74603
Apricato <100396000+Apricato@users.noreply.github.com>
2025-09-15 14:12:01 -0600

    Parcial01- Display fetched users

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, signal, inject, OnInit} from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+
+
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  protected members = signal<any>([]);
+
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(): Promise<Object>  {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
